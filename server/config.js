57dc600f52518f0751bac68c6be1d1c13e1bb5b2
Christof <bauer.christof@gmail.com>
2026-02-13 16:11:16 +0100

    Update config.js

--- server/config.js
+++ server/config.js
@@ -2,5 +2,5 @@
  * Server config from environment. Required vars are validated at startup in index.js.
  * Use this module instead of process.env so all config is read from one place.
  */
-export const MINER_IP = process.env.MINER_IP ?? '';
+export const MINER_IP = process.env.MINER_IP ?? '192.168.1.3';
 export const PORT = process.env.PORT ?? '8001';
